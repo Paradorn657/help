68a977f9ef867ed3ddb7a9eba409ac3533ecea80
Paradorn657 <58761409+Paradorn657@users.noreply.github.com>
2020-03-19 16:23:36 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -23,8 +23,6 @@ y = tf.nn.relu(xph @ w1 + b1)
 y = tf.nn.relu(y @ w2 + b2)
 y = y @ w3 + b3
 
-y_true = tf.placeholder(tf.float32, shape=[None, 10])
-
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=yph, logits=y))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(cross_entropy)
@@ -35,8 +33,5 @@ with tf.Session() as sess:
     for step in range(100):
         batch_x, batch_y = mnist.train.next_batch(150)
         sess.run(train, feed_dict={xph: batch_x, yph: batch_y})
-    matches = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1))
-    acc = tf.reduce_mean(tf.cast(matches, tf.float32))
-    print(sess.run(acc, feed_dict={xph: mnist.test.images, y_true: mnist.test.labels}))
     w_1, b_1, w_2, b_2, w_3, b_3 = sess.run([w1, b1, w2, b2, w3, b3])
 
